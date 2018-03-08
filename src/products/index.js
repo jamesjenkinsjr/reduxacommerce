f5b100fc057d994598fd1e87b320d1e2d13c0294
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-08 18:55:59 -0500

    Stub out reducers and use combineReducer in store

---
+++ src/products/index.js
@@ -0,0 +1,6 @@
+const reducer = (state={}, action) => {
+    console.log('products stuff');
+    return state;
+}
+
+export default reducer;
---
+++ src/search/index.js
@@ -0,0 +1,6 @@
+const reducer = (state={}, action) => {
+    console.log('search stuff');
+    return state;
+}
+
+export default reducer;
