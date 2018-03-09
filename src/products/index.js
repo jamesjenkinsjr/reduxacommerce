09586e5360cc108ca09f94455b6da09e50cb4d2f
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-08 19:27:35 -0500

    Create mock state for products

--- src/products/index.js
+++ src/products/index.js
@@ -1,4 +1,25 @@
-const reducer = (state={}, action) => {
+const initialState = [
+    {
+        imgSource: 'https://via.placeholder.com/100x100',
+        name: 'Javelin (CAUTION)',
+        price: 500.00,
+        count: 0
+    },
+    {
+        imgSource: 'https://via.placeholder.com/100x100',
+        name: 'Boogie Board',
+        price: 100.00,
+        count: 0 
+    },
+    {
+        imgSource: 'https://via.placeholder.com/100x100',
+        name: 'Loch Ness Monster',
+        price: 350.00,
+        count: 0
+    }
+];
+
+const reducer = (state=initialState, action) => {
     console.log('products stuff');
     return state;
 }
