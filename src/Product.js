987eede8ce59d2e4e5136c777803cdd16f18a3c1
James Jenkins <jamesjenkinsjr@icloud.com>
2018-03-12 18:32:12 -0400

    Fix ADD_ITEM to use new id in actions and reducer for products

--- src/Product.js
+++ src/Product.js
@@ -8,17 +8,17 @@ const Product = props => {
             <img src={props.imgSource} alt={props.name}/>
             <p>{props.name}</p>
             <p>${props.price}</p>
-            <button onClick={() => props.addItem(props.position)}>+</button>
+            <button onClick={() => props.addItem(props.id)}>+</button>
             <p>{props.count}</p>
-            <button onClick={() => props.removeItem(props.position)}>-</button>
+            <button onClick={() => props.removeItem(props.id)}>-</button>
         </li>
     );
 }
 const mapStateToProps = null;
 const mapDispatchToProps = dispatch => {
     return {
-        addItem: position => dispatch(productActions.addItem(position)),
-        removeItem: position => dispatch(productActions.removeItem(position))
+        addItem: id => dispatch(productActions.addItem(id)),
+        removeItem: id => dispatch(productActions.removeItem(id))
     };
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Product);
